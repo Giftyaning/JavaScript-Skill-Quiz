c7826fc8930c99e820a28eaf9323ec03dd191091
Gifty <giftyaningg@gmail.com>
2024-02-29 17:52:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function beginQuiz() {
     firstIntro.style.display = "none";
     quizQuestion.style.display = "block"
 }
+
+//Const variables
+const timer = document.getElementById("time");
+const completedQuestion = document.getElementById("completed");
+const remainingQuestion = document.getElementById("remaining");
+const questions = document.getElementById("question");
+const answerBtn = document.getElementById("answer");
+
