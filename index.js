afe9dcb883f55563c803a9019d492a20c96650ba
Gifty <giftyaningg@gmail.com>
2024-03-01 23:21:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Display quiz question when the Start quiz button is clicked
-function beginQuiz() {
+/*function beginQuiz() {
     var firstIntro = document.getElementById("first-intro");
     var quizQuestion = document.getElementById("quiz");
 
     firstIntro.style.display = "none";
     quizQuestion.style.display = "block";
     displayQuestion(currentQuestion);
-}
+}*/
 
 //Const variables
 const timer = document.getElementById("time");
